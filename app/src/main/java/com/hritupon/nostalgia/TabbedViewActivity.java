18ce63762e2898cfa069d617887392ad94abc04d
hritupon <hritupon.saikia@gmail.com>
2017-09-02 03:15:55 +0530

    floating button added

--- app/src/main/java/com/hritupon/nostalgia/TabbedViewActivity.java
+++ app/src/main/java/com/hritupon/nostalgia/TabbedViewActivity.java
@@ -1,10 +1,13 @@
 package com.hritupon.nostalgia;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class TabbedViewActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class TabbedViewActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
     ViewPagerAdapter viewPagerAdapter;
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +29,12 @@ public class TabbedViewActivity extends AppCompatActivity {
         viewPagerAdapter.addFragments(new CalendarActivity(), "Calendar");
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TabbedViewActivity.this, SpeechRecordActivity.class));
+            }
+        });
     }
 }
